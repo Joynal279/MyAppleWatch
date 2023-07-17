53c2c3dcf8e9a99c8d75a542f6e7089d8517249f
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-07-17 07:31:07 +0600

    Initial Commit

---
+++ MyAppleWatch Watch App/MyAppleWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppleWatchApp.swift
+//  MyAppleWatch Watch App
+//
+//  Created by MAC on 17/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MyAppleWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
